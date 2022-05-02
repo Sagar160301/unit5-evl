33facf6bd80d6abe7c39c1a812b5d82788563dd6
Sagar@16 <sagarkumarpradhan2001@gmail.com>
2022-05-02 16:44:30 +0530

    first

---
+++ c4-evl/src/components/NewOrder.jsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export const NewOrder = () => {
+  const [data, setData] = useState([]);
+
+  const [filt, setfilt] = useState(false);
+
+  const name = useSelector((store) => store.user.username);
+  const auth = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    owner_name: name,
+    status: "Not Accepted",
+  });
+  useEffect(() => {
+    if (auth == false) {
+      navigate("/login");
+    }
+    getData();
+  }, []);
+  //   console.log(name);
+  const getData = async () => {
+    let res = await fetch(`http://localhost:8080/orders?owner_name=${name}`);
+    let data = await res.json();
+    if (filt === true) {
+      let arr = data.filter((el) => {
+        return el.status != "Done";
+      });
+      console.log(arr);
+      setData(arr);
+    } else {
+      setData(data);
+    }
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //post data
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // console.log(user);
+    setUser({ ...user, [name]: value });
+  };
+
+  const submitData = async () => {
+    try {
+      let res = await fetch("http://localhost:8080/orders", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      let data = await res.json();
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Get data of only this user. store it in redux
+  // GET /orders?owner_name=john will give you all order of user john
+  //  on submit click create a new order, new order has status `Not Accepted`
+  return (
+    <div>
+      <div className="form">
+        <input
+          className="new-problem"
+          type="text"
+          name="problem"
+          onChange={handleChange}
+          placeholder="Enter problem"
+        />
+        {/* This input is readonly, it's coming from redux */}
+        <input
+          className="owner-name"
+          type="text"
+          value={name}
+          name="owner_name"
+          onChange={handleChange}
+          placeholder="yourname"
+          readOnly
+        />
+        <input
+          className="brand"
+          type="text"
+          name="brand"
+          onChange={handleChange}
+          placeholder="Enter brand name"
+        />
+        {/* Create new problem, show it in below form immediately */}
+        <button className="submit" onClick={submitData}>
+          submit
+        </button>
+      </div>
+
+      <div className="pastOrders">
+        {/* this button filters the data below. */}
+        {/* it's just a toggle of redux state something like `showUnfinished`  */}
+        <button
+          className="filter"
+          onClick={() => {
+            console.log(filt);
+            setfilt(!filt);
+            getData();
+          }}
+        >
+          {filt === false ? "Show All" : "Show Only Unfinished"}
+          {/* Text should change like:   Show {showUnfinished ? "all" : "Only unfinished"} */}
+        </button>
+
+        {/* Here create a div for every oreder, filter them before based on `showUnfinished` */}
+        <div className="past-orders">
+          {data.length == 0
+            ? null
+            : data.map((el) => {
+                return (
+                  <div key={el.id}>
+                    <span className="id">{el.id}</span>.{" "}
+                    <span className="problem">{el.problem}</span>{" "}
+                    <span className="cost">
+                      {el.status === "Not Accepted" ? "---" : `$ ${el.cost}`}
+                      {/* if status is not accepted then keep it empty otherwise show cost like $1234 */}
+                    </span>
+                    <p className="status">Status:{el.status} </p>
+                    <hr />
+                  </div>
+                );
+              })}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ c4-evl/src/components/Orders.jsx
@@ -0,0 +1,150 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { AllData } from "../Redux/actions";
+
+export const Orders = () => {
+  const [rand, setR] = useState(true);
+  const [allData, setAllData] = useState([]);
+  const dispatch = useDispatch();
+  const auth = useSelector((store) => store.auth);
+  let all = useSelector((store) => store.all);
+  console.log(all, "all");
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (auth == false) {
+      navigate("/login");
+    }
+    getData();
+  }, []);
+  //  Get all data when admin logs in and populate it
+  // store it in redux
+  const getData = async () => {
+    try {
+      let res = await fetch("http://localhost:8080/orders");
+      let data = await res.json();
+      dispatch(AllData(data));
+      setAllData(all);
+      setR(!rand);
+      //   console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const changeStatus = async (id, key, value) => {
+    let obj = { [key]: value };
+    try {
+      let res = await fetch(`http://localhost:8080/orders/${id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      });
+      let data = await res.json();
+      console.log(data, "patch");
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const changeCost = async (id, key, value) => {
+    let obj = { [key]: value };
+    try {
+      let res = await fetch(`http://localhost:8080/orders/${id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(obj),
+      });
+      let data = await res.json();
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <select className="controls" name="progress" id="progress">
+            <option value="id">ID</option>
+            <option value="status">Status</option>
+            <option value="cost">Cost</option>
+          </select>
+        </div>
+        <table className="orders">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Problem</th>
+              <th>Client Name</th>
+              <th>Status</th>
+              <th>Cost</th>
+              <th>Change Status</th>
+              <th>Accept</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allData.length == 0
+              ? null
+              : allData.map((el) => {
+                  return (
+                    <tr className="orders-row" key={el.id}>
+                      <td className="id">{el.id}</td>
+                      <td className="problem">{el.problem}</td>
+                      <td className="owner">{el.owner_name}</td>
+                      <td className="status">{el.status}</td>
+                      <td className="cost">
+                        {el.cost == undefined ? "-" : `$  ${el.cost}`}
+                      </td>
+                      <td className="change-status">
+                        {/* Show select dropdown only if status is Not Accepted */}
+                        {el.status == "Not Accepted" ? null : (
+                          <select
+                            className="changeStatus"
+                            onChange={(e) => {
+                              changeStatus(el.id, "status", e.target.value);
+                            }}
+                            name="changeStatus"
+                          >
+                            <option value="Pending">Pending</option>
+                            <option value="In Progress">In Progress</option>
+                            <option value="Done">Done</option>
+                            <option value="Not Accepted">Not Accepted</option>
+                          </select>
+                        )}
+                      </td>
+                      <td className="accept">
+                        {el.status == "Not Accepted" ? (
+                          <button
+                            onClick={(e) => {
+                              changeCost(
+                                e.id,
+                                "cost",
+                                Math.round(Math.random(1, 1000))
+                              );
+                              changeStatus(el.id, "status", "In Progress");
+                            }}
+                          >
+                            Accept
+                          </button>
+                        ) : (
+                          ""
+                        )}
+                        {/* Show this button only if status is Not Accepted */}
+                        {/* on change make request to update it in db, and show changed status in table */}
+                      </td>
+                    </tr>
+                  );
+                })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
